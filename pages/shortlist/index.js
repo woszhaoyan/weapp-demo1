c4fc4a954ac7de36a14e986fbf2c2b3f96875f9c
zhaoyan.joy <zhaoyan.joy@bytedance.com>
2020-03-07 20:25:57 +0800

    添加购物车

--- pages/shortlist/index.js
+++ pages/shortlist/index.js
@@ -5,62 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goods: []
+    goods: [{ "image":"../../images/mt.jpg", "title":"我在测试一下前端", "number":1, "price":5000},
+      {"image": "../../images/mt.jpg", "title": "我在测试一下前端", "number": 1, "price": 5000},
+      {"image": "../../images/mt.jpg", "title": "我在测试一下前端", "number": 1, "price": 5000}],
+    isloading:false,
+    checked: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onChange(event) {
+    this.setData({
+      checked: event.detail
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onClickButton:function(){
+    var that = this.data.isloading;
+    this.setData({isloading : !that})
   }
+
 })
