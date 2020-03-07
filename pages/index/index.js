99c83faa65e9dbd5eed8c26cdeb732acee97d413
zhaoyan.joy <zhaoyan.joy@bytedance.com>
2020-03-07 17:59:40 +0800

    框架搭建

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: ["../../images/mt.jpg","../../images/mt.jpg"],
+    newCommoditys: [{ "image":"../../images/zy.jpg","title":"","original_price":1000, "price": 500,"sold":50},
+      { "image": "../../images/zy.jpg", "title": "", "original_price": 1000, "price": 500, "sold": 50 },
+      { "image": "../../images/zy.jpg", "title": "", "original_price": 1000, "price": 500, "sold": 50 },
+      { "image": "../../images/zy.jpg", "title": "", "original_price": 1000, "price": 500, "sold": 50 }],
+  }
+  
+  
+})
