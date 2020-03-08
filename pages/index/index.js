5b9a96452febddc330d256e8a980738cf9caca1a
zhaoyan.joy <zhaoyan.joy@bytedance.com>
2020-03-08 15:41:47 +0800

    last

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,5 +16,18 @@ Page({
       { "id": 3, "image": "http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg", "title": "三大火力科技,品质升级,让烹饪更美好", "model": "JZ(Y/T/R)-9B32", "price": 500, "sold": 50 }],
   },
 
-  
+  toDetail :function(){
+
+    wx.navigateTo({
+      url: '../../pages/detail/index',
+      success:function(res){
+        console.log("zhaoyan success")
+      },
+      fail:function(res){
+        console.log(res)
+      }
+    })
+
+  }
+
 })
