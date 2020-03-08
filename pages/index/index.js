6b1f6ef2266d397501d2fe515c964dec01fb83ea
zhaoyan.joy <zhaoyan.joy@bytedance.com>
2020-03-08 15:17:43 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: ["../../images/mt.jpg","../../images/mt.jpg"],
-    newCommoditys: [{ "image":"../../images/zy.jpg","title":"","original_price":1000, "price": 500,"sold":50},
-      { "image": "../../images/zy.jpg", "title": "", "original_price": 1000, "price": 500, "sold": 50 },
-      { "image": "../../images/zy.jpg", "title": "", "original_price": 1000, "price": 500, "sold": 50 },
-      { "image": "../../images/zy.jpg", "title": "", "original_price": 1000, "price": 500, "sold": 50 }],
+    imgUrls: ["http://q6v0uxmjt.bkt.clouddn.com/slide1.png", 
+              "http://q6v0uxmjt.bkt.clouddn.com/slide2.png",
+              "http://q6v0uxmjt.bkt.clouddn.com/slide3.png"],
+    newCommoditys: [{ "id": 1, "image":"http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg","title":"三大火力科技,品质升级,让烹饪更美好","model":"JZ(Y/T/R)-9B32", "price": 500,"sold":50},
+      { "id": 2, "image": "http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg", "title": "三大火力科技,品质升级,让烹饪更美好", "model": "JZ(Y/T/R)-9G38", "price": 500, "sold": 50 },
+      { "id": 3, "image": "http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg", "title": "三大火力科技,品质升级,让烹饪更美好", "model": "JZ(Y/T/R)-9B32", "price": 500, "sold": 50 },
+      { "id": 1, "image": "http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg", "title": "三大火力科技,品质升级,让烹饪更美好", "model": "JZ(Y/T/R)-9B32", "price": 500, "sold": 50 },
+      { "id": 2, "image": "http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg", "title": "三大火力科技,品质升级,让烹饪更美好", "model": "JZ(Y/T/R)-9G38", "price": 500, "sold": 50 },
+      { "id": 3, "image": "http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg", "title": "三大火力科技,品质升级,让烹饪更美好", "model": "JZ(Y/T/R)-9B32", "price": 500, "sold": 50 },
+      { "id": 3, "image": "http://q6v0uxmjt.bkt.clouddn.com/1560244061502LMwSERt6.jpg", "title": "三大火力科技,品质升级,让烹饪更美好", "model": "JZ(Y/T/R)-9B32", "price": 500, "sold": 50 }],
   },
-
 
   
 })
